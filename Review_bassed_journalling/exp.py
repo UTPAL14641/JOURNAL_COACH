68716d3b855859f29831966fa1d8f2dc99bb2cff
UTPAL RAJ <104608445+UTPAL14641@users.noreply.github.com>
2024-05-31 00:27:38 +0530

    requirements

--- Review_bassed_journalling/exp.py
+++ Review_bassed_journalling/exp.py
@@ -1,4 +1,4 @@
-import re
+'''import re
 from typing import Tuple, List
 
 from langchain_openai import OpenAIEmbeddings
@@ -20,4 +20,30 @@ client = OpenAI(
 embeddings = OpenAIEmbeddings()
 question="Why I no longer feel motivated to work"
 db = FAISS.load_local("faiss_index_ques_200_50", embeddings,allow_dangerous_deserialization =True)
-print(db.similarity_search(question, k=4))
+print(db.similarity_search(question, k=4))'''
+import pkg_resources
+
+def get_installed_version(package_name):
+    try:
+        return pkg_resources.get_distribution(package_name).version
+    except pkg_resources.DistributionNotFound:
+        return None
+
+def main():
+    try:
+        with open('requirements.txt', 'r') as file:
+            packages = file.readlines()
+    except FileNotFoundError:
+        print("The requirements.txt file was not found.")
+        return
+
+    for package in packages:
+        package_name = package.strip().split('==')[0]  # Remove any version specifier
+        version = get_installed_version(package_name)
+        if version:
+            print(f"{package_name}: {version}")
+        else:
+            print(f"{package_name}: Not installed")
+
+if __name__ == "__main__":
+    main()
